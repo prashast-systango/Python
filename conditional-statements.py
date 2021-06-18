1d7fe462bdae9bfa6aea360aada21dbc7f46a0e4
prashast-systango <psitlani@isystango.com>
2021-06-18 20:33:40 +0530

    Conditional Statements

---
+++ conditional-statements.py
@@ -0,0 +1,12 @@
+a = 42
+b=[1,2,3,4,5]
+if(a is 56):
+    print("yes a is 56")
+elif(a > 56):
+    print("no")
+elif(5 in b):
+    if(4 in b):
+        print("yes 4 and 5 both in b")
+    else:
+        print("yes 5 in b")
+            
