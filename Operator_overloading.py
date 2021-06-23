ac34d75fcefefd98cf221348df66794433baafc6
prashast-systango <psitlani@isystango.com>
2021-06-23 22:17:13 +0530

    Python Operator Overloading

---
+++ Operator_overloading.py
@@ -0,0 +1,12 @@
+def sum():
+    a = int(input("Enter any number :"))
+    b = int(input("Enter another number :"))
+    return print(a + b)
+    
+def concat():
+    a = input("Enter anything :")
+    b = input("Enter anything :")
+    return print(a + b)
+
+# concat()  #concat using '+'
+# sum()  #sums using '+'
